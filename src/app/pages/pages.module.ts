8093363847100a2c2af0e20ead95b047aee010f0
taliho2021 <tyyo1980@gmail.com>
2021-12-27 15:23:32 -0500

    Added Angular Material to pages.module

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,4 +1,6 @@
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button'
+import { MatCardModule } from '@angular/material/card';
 import { NgModule } from '@angular/core';
 import { PagesComponent } from './pages.component';
 import { PagesRoutingModule } from './pages-routing.module';
@@ -10,6 +12,8 @@ import { PagesRoutingModule } from './pages-routing.module';
   imports: [
     CommonModule,
     PagesRoutingModule,
+    MatCardModule,
+    MatButtonModule
   ]
 })
 export class PagesModule { }
