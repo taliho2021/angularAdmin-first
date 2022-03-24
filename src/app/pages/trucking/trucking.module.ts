ca42c9f25361049a99dceb18500e604d3d38ee09
taliho2021 <tyyo1980@gmail.com>
2022-03-24 17:38:53 -0400

    Updated delivery module

--- src/app/pages/trucking/trucking.module.ts
+++ src/app/pages/trucking/trucking.module.ts
@@ -4,6 +4,7 @@ import { AirComponent } from './air/air.component';
 import { CommonModule } from '@angular/common';
 import { FclComponent } from './fcl/fcl.component';
 import { LclComponent } from './lcl/lcl.component';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { NgModule } from '@angular/core';
 import { TruckingComponent } from './trucking.component';
@@ -19,6 +20,7 @@ import { TruckingRoutingModule } from './trucking-routing.module';
   imports: [
     CommonModule,
     MatCardModule,
+    MatButtonModule,
     MatGridListModule,
     TruckingRoutingModule
   ]
