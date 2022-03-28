cb8c7c1310d060e6f24fe4a5ceca724491b6fd71
Tal Yo <tal@analink.com>
2022-03-28 09:08:28 -0400

    Updated components

--- src/app/pages/track-and-trace/track-and-trace-routing.module.ts
+++ src/app/pages/track-and-trace/track-and-trace-routing.module.ts
@@ -1,12 +1,22 @@
 import { RouterModule, Routes } from '@angular/router';
 
+import { AirExportComponent } from './air-export/air-export.component';
 import { AirImportComponent } from './air-import/air-import.component';
+import { EntriesComponent } from './entries/entries.component';
 import { NgModule } from '@angular/core';
+import { OceanExportComponent } from './ocean-export/ocean-export.component';
+import { OceanImportComponent } from './ocean-import/ocean-import.component';
 import { TrackAndTraceComponent } from './track-and-trace.component';
 
 const routes: Routes = [
-  { path: '', component: TrackAndTraceComponent },
-  { path: 'airImport', component: AirImportComponent}];
+  { path: '', component: TrackAndTraceComponent, children: [
+      { path: 'airImport', component: AirImportComponent},
+      { path: 'airExport', component: AirExportComponent},
+      { path: 'oceanImport', component: OceanImportComponent},
+      { path: 'oceanExport', component: OceanExportComponent},
+      { path: 'entries', component: EntriesComponent}
+  ],
+  }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/pages/track-and-trace/track-and-trace.module.ts
+++ src/app/pages/track-and-trace/track-and-trace.module.ts
@@ -5,11 +5,19 @@ import { NgModule } from '@angular/core';
 import { TrackAndTraceComponent } from './track-and-trace.component';
 import { TrackAndTraceRoutingModule } from './track-and-trace-routing.module';
 import { AirImportComponent } from './air-import/air-import.component';
+import { AirExportComponent } from './air-export/air-export.component';
+import { OceanImportComponent } from './ocean-import/ocean-import.component';
+import { OceanExportComponent } from './ocean-export/ocean-export.component';
+import { EntriesComponent } from './entries/entries.component';
 
 @NgModule({
   declarations: [
     TrackAndTraceComponent,
-    AirImportComponent
+    AirImportComponent,
+    AirExportComponent,
+    OceanImportComponent,
+    OceanExportComponent,
+    EntriesComponent
   ],
   imports: [
     CommonModule,
