c374cab717a4bf7b330fb09285874465a3b5e58d
Tal Yo <tal@analink.com>
2022-07-26 20:14:34 -0400

    updated hts component

--- src/app/pages/tables/hts/hts.component.ts
+++ src/app/pages/tables/hts/hts.component.ts
@@ -1,50 +1,37 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
-import { MatTable } from '@angular/material/table';
-import { HTSDataSource, HTSItem } from './hts-datasource';
-import { ColDef, GridApi,  RowClickedEvent } from 'ag-grid-community';
+import { Component } from '@angular/core';
 
+interface HTS {
+  seqNo: Number;
+  refNo: number;
+}
 @Component({
   selector: 'app-hts',
   templateUrl: './hts.component.html',
-  styleUrls: ['./hts.component.scss']
+  styleUrls: ['./hts.component.scss'],
 })
+export class HTSComponent {
+  rowData: HTS[] = [];
 
-interface HTS  {
-  seqNo : Number,
-  refNo: number,
-
-}
-export class HTSComponent  {
- 
- rowData: HTS[] = [];
-
- gridOptions = {
+  gridOptions = {
     rowModelType: 'serverside',
 
     columnDefs: [
-      {field: 'other'},
-      {field: 'superior'},
-      {field: 'description'},
-      {field: 'quotaQuantity'},
-      {field: 'special'},
-      {field: 'htsno'},
-      {field: 'additionalDuties'},
-      {field: 'footnote'},
-      {field: 'general'},
-      {field: 'units'},
-
+      { field: 'other' },
+      { field: 'superior' },
+      { field: 'description' },
+      { field: 'quotaQuantity' },
+      { field: 'special' },
+      { field: 'htsno' },
+      { field: 'additionalDuties' },
+      { field: 'footnote' },
+      { field: 'general' },
+      { field: 'units' },
     ],
 
-    defaultColDef : {
-      sortable: true
-    }
- } 
+    defaultColDef: {
+      sortable: true,
+    },
+  };
 
-  constructor() {
-   
-  }
-
-  
+  constructor() {}
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,28 +2,27 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-const API_URL = 'http://localhost:3000/api/test/';
+const API_URL = 'https://nodejs01-app.herokuapp.com/';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
-
-  constructor(private htttp: HttpClient) { }
+  constructor(private htttp: HttpClient) {}
 
   getPublicContent(): Observable<any> {
-    return this.htttp.get(API_URL + 'all', { responseType: 'text'} );
+    return this.htttp.get(API_URL + 'all', { responseType: 'text' });
   }
 
   getUserBoard(): Observable<any> {
-    return this.htttp.get(API_URL + 'user', {responseType: 'text'});
+    return this.htttp.get(API_URL + 'user', { responseType: 'text' });
   }
 
   getModeratorBoard(): Observable<any> {
-    return this.htttp.get(API_URL + 'mod', {responseType: 'text'});
+    return this.htttp.get(API_URL + 'mod', { responseType: 'text' });
   }
 
   getAdminBoard(): Observable<any> {
-    return this.htttp.get(API_URL + 'admin', {responseType: 'text'});
+    return this.htttp.get(API_URL + 'admin', { responseType: 'text' });
   }
 }
