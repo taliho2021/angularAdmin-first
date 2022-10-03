a4502ed69890a97a6a6fe2596fe591a1b5ffdda8
taliho2021 <tal@analink.com>
2022-10-03 17:50:51 -0400

    Created cust.ts interface

---
+++ src/app/models/cust.ts
@@ -0,0 +1,19 @@
+export class Customer {
+  seqNo!: number;
+  custId!: string;
+  custCode!: string;
+  custName!: string;
+  addressLine1!: string;
+  addressLine2!: string;
+  city!: string;
+  state!: string;
+  zipCode!: string;
+  country!: string;
+  telNo!: string;
+  faxNo!: string;
+  contact1!: string;
+  email1!: string;
+  einNo!: string;
+  payorUnitNo!: string;
+  creditLimit!: number;
+}
